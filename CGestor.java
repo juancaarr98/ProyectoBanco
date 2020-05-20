6e6092172eeedeaf7d2a91a94ffc4b960e7ed1c0
Juan  carlos <Juan  carlos@DESKTOP-TPUH706>
2020-05-20 22:59:10 +0200

    creacion de clase CGestor TERMINADA

--- CGestor.java
+++ CGestor.java
@@ -14,9 +14,28 @@ public class CGestor {
    
     public CGestor() {}
     
-    public void mvCrearTarjeta(CCuenta cu,int limt_cajero,int  limt_on,int codigo){}
-    public void mvCrearCuenta(CCliente a,int dinero,String fecha_alta){}
-    public void mvCrearPerfil(String nom,String apell,String d,String dni,int age,int tlf){}
-    public void mvModificarDatos(CCliente a,String nom,String apell,String d,String dni,int tlf,int age){}
-    public void mvConsultar(CCliente a){}
+    public void mvCrearTarjeta(CCuenta cu,int limt_cajero,int  limt_on,int codigo){
+        CTarjeta t= new CTarjeta(limt_cajero,limt_on,codigo);
+        cu.getAlTarjetas().add(t);
+    }
+    public void mvCrearCuenta(CCliente a,int dinero,String fecha_alta){
+        CCuenta c=new CCuenta(dinero,fecha_alta);
+        a.getAlCuentas().add(c);
+        System.out.println("Esta cuennta, todavia no tiene tarjetas");
+    }
+    public void mvCrearPerfil(String nom,String apell,String d,String dni,int age,int tlf){
+        CCliente p=new CCliente(nom,apell,d,dni,age,tlf);
+        System.out.println("Este perfil no tiene cuentas asociadas.");       
+    }
+    public void mvModificarDatos(CCliente a,String nom,String apell,String d,String dni,int tlf,int age){
+        a.setNombre(nom);
+        a.setAplellido(apell);
+        a.setDir(d);
+        a.setNif(dni);
+        a.setTf(tlf);
+        a.setEdad(age);
+    }
+    public void mvConsultar(CCliente a){
+        a.toString(); 
+    }
 }
